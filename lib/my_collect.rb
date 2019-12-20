cd7a9d86940d457c9a22b3489fc2165284985129
Will Carter <will.carter@fergusllc.com>
2019-12-20 23:31:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+  i = 0
+  return_array = []
+  while i < array.length
+    return_array << yield
+    i = i + 1
+  end
+  return_array
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+puts my_collect(array) { |name| name.split(" ").first }
